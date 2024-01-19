cdd5b4ecb96f2c278922c5c16d46194f502da552
tafforzore <nzomutchawilfrid@gmail.com>
2024-01-19 05:55:59 +0100

    sev

--- Makefile
+++ Makefile
@@ -4,7 +4,10 @@ CFLAGS = -Wall -Wextra -std=c11
 all: main
 
 main: main.c fonctions.c
-	$(CC) $(CFLAGS) -o main main.c fonctions.c
+	$(CC) $(CFLAGS) -o main main.c fonctions.c -lm
+
+run: main
+	./main
 
 clean:
 	rm -f main
--- main.c
+++ main.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h> // Ajoutez cette ligne
 #include "fonctions.h"
 
 int main() {
-    char text[2000]; 
-    char pattern[100];  
+    char text[1000];
+    char pattern[100];
 
-    printf("veuillez entrer le texte : ");
+    printf("Veuillez entrer le texte : ");
     fgets(text, sizeof(text), stdin);
+
     if (text[strlen(text) - 1] == '\n') {
         text[strlen(text) - 1] = '\0';
     }
 
-    printf("veuillez entrer le motif à rechercher : ");
+    printf("Veuillez entrer le motif à rechercher : ");
     scanf("%s", pattern);
 
     int occurrences_with_automaton = count_occurrences_with_automaton(pattern, text);
     int occurrences_sequential = count_occurrences_sequential(pattern, text);
 
-    printf("occurrences du motif dans le texte (avec automate) : %d\n", occurrences_with_automaton);
-    printf("occurrences du motif dans le texte (sans automate) : %d\n", occurrences_sequential);
+    printf("Occurrences du motif dans le texte (avec automate) : %d\n", occurrences_with_automaton);
+    printf("Occurrences du motif dans le texte (sans automate) : %d\n", occurrences_sequential);
 
     return 0;
 }
